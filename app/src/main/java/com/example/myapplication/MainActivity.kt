32f2f09bf8da26cb0630d27bfc1cf36e2cc76f9a
DinarPerseus <dinarmridha@gmail.com>
2025-03-24 12:38:28 +0600

    Logout process complete

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        FirebaseApp.initializeApp(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.profile
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -10,12 +11,16 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.example.myapplication.Login
+import com.example.myapplication.MainActivity2
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
@@ -23,12 +28,13 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        val btnLogout = binding.btnlogout
         val imageView: ImageView = binding.imageView
         val animZoomInOut: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_in_out)
-
+        auth = FirebaseAuth.getInstance()
         imageView.setOnTouchListener{ _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
@@ -42,6 +48,15 @@ class ProfileFragment : Fragment() {
                 }
                 else -> false
             }
+        }
+
+        btnLogout.setOnClickListener {
+            // Handle logout button click
+            auth.signOut()
+            val intent = Intent(requireContext(), MainActivity2::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+
         }
 
         return root
